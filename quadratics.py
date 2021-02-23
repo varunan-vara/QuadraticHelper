164180f860f61a22c4ec8ebb6527293a53afdc09
Varunan_V <47948397+varunan-vara@users.noreply.github.com>
2021-02-23 09:07:15 -0500

    Added method to find x ints

--- quadratics.py
+++ quadratics.py
@@ -6,35 +6,42 @@ import vertex
 import argparse
 
 #Setup args and opts
-opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+opts = [opt for opt in sys.argv[1:] if opt.startswith("--")]
 
 #Testing Area
 
-#Argparse prep
-parser = argparse.ArgumentParser(description = "Solve Quadratic Equations.")
-parser.add_argument("--vertex", dest="accumulator", action="store_const", const=sum, default=max, help="Find the vertex of a quadratic equation, given ax^2 + bx + c")
-args = parser.parse_args()
-print(args.accumulate(args.integers))
+
 
 #Performer Functions
 def vertexfinder():
-	print("Finiding Vertex:\nEquation formatted as: ax^2 + bx + c\n\n")
+	print("Finding Vertex:\nEquation formatted as: ax^2 + bx + c\n\n")
 	a = int(input("a = "))
 	b = int(input("b = "))
 	c = int(input("c = "))
 	coord = vertex.vertex(a, b, c)
 	print("Vertex at: ({}, {})".format(coord[0], coord[1]))
-def helpy():
-	print("This is a command line tool to help solve quadratic style equations.\nTo see what type of commands are available, run without any arguments\nNo Dependencies Required\nMade by Varunan Varathan")
-
+def interceptfinder():
+	print("Finding Vertex:\nEquation formatted as: ax^2 + bx + c\n\n")
+	a = int(input("a = "))
+	b = int(input("b = "))
+	c = int(input("c = "))
+	intercepts = vertex.xint(a, b, c)
+	print("X intercepts at: ({}, {})".format(intercepts[0], intercepts[1]))
 #Doer
 if (len(opts) > 1):
 	print("Please input one argument. Inputted argument: {}".format(opts))
 	sys.exit()
 
-if "-vertex" in opts:
+if "--vertex" in opts:
 	vertexfinder()
-elif "-help" in opts:
-	helpy()
+elif "--xint" in opts:
+	interceptfinder()
+elif "--help" in opts:
+	#Argparse prep
+	parser = argparse.ArgumentParser(description = "This is a command line tool to help solve quadratic style equations;   \nNo Dependencies Required; \nMade by Varunan Varathan")
+	parser.add_argument("--vertex", help="Find the vertex of a quadratic equation")
+	parser.add_argument("--xint", help="Find the x intercepts of a quadratic equation")
+	args = parser.parse_args()
+	print(args.accumulate(args.integers))
 
 
--- vertex.py
+++ vertex.py
@@ -1,4 +1,5 @@
 import sys 
+import math
 # Find the vertex of a parabola
 def vertex(a, b, c):
 	xcord = 0 - (b / (2 * a))
@@ -11,13 +12,15 @@ def xint(a, b, c):
 	firstcoord = a + b + c
 	if (vertexcoord[1] > 0):
 		if (firstcoord > vertexcoord[1]):
-			return "No x interecepts present"
+			return ["No x interecepts present", "No x interecepts present"]
 			pass
 	else:
 		if firstcoord < vertexcoord[1]:
-			return "No x interecepts present"
+			return ["No x interecepts present", "No x interecepts present"]
 			pass
-	
+	intercept1 = ((0 - b) + math.sqrt((b * b) - (4 * a * c))) / (2 * a)
+	intercept2 = ((0 - b) - math.sqrt((b * b) - (4 * a * c))) / (2 * a)
+	return [intercept1, intercept2]
 
 # Find y intercepts
 def yint(a, b, c):
